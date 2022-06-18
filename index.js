b098fc3516100bdf08cab820372e9531ef9892fb
Gamers-geek <contact@gamers-geek.gq>
2022-06-18 15:38:18 +0200

    Système de debug + "base" handler

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-const ws = require("ws")
-const { port } = require('config.js')
-const server = new ws.Server({ port: port})
+const { Server, WebSocket } = require("ws");
+const { readdirSync } = require("fs");
+const { port, debug } = require('config');
+const { debug } = require('utils/debug')
+const server = new Server({ port: port });
 
 server.on("connection", client => {
     client.on("message", message => {
-        try{
-            let data = JSON.parse(message)
-
-            console.log(data)
-        } catch(error) {
-            console.log(error)
+        try {
+            handleOnMessageReceive(message)
+        } catch (error) {
+            console.log(error);
         };
-    })
+    });
     client.on("close", (code, reason) => {
-        console.log(code, reason)
-    })
-    client.send(JSON.stringify({"test": "test"}))
-})
+        console.log(code, reason);
+    });
+    client.send(JSON.stringify({ "test": "test" }));
+});
+
+/**
+ * @param {JSON} message 
+ * @param {WebSocket} socket 
+ * @returns {void}
+ */
+async function handleOnMessageReceive(message, socket) {
+    const parsedMessage = JSON.parse(message);
+    if (!parsedMessage) {
+        return console.error("[ERROR] Invalid message format", message);
+    };
+
+    if (debug) {
+        debug("[DEBUG] Received message", parsedMessage);
+    }
+
+    return;
+};
